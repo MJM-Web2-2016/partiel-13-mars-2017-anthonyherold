c4e3eba53455c2af0cde944970e32ae9e9003052
anthonyherold <herold.anthony@gmail.com>
2017-03-14 18:58:08 +0100

    Commit-1

--- wp-content/themes/FoundationPress-master/index.php
+++ wp-content/themes/FoundationPress-master/index.php
@@ -14,36 +14,32 @@
  */
 
 get_header(); ?>
+<section class="top">
+	<div class="background-top">
+		<h1> Musique <br /> Action <br /> Festival</h1>
+	</div>
+</section>
 
-<div id="page" role="main">
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
-
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; // End have_posts() check. ?>
-
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
-
-	</article>
-	<?php get_sidebar(); ?>
-
-</div>
+<section class="programme">
+	<h2>Programme</h2>
+	<div class="row">
+		<div class="large-3 columns contour">
+			<p>Mercredi <br />03 Mai 2017<br /><strong>Médiathèque</strong><br />18h<br /></p>
+			<h3>CIE OUÏE/DIRE</h3>
+		</div>
+		<div class="large-3 columns contour">
+			<p>Mercredi <br />05 Mai 2017<br /><strong>St-Léon - Nancy</strong><br />18h<br /></p>
+			<h3>CHARLÈNE <br />MARCHAND</h3>
+		</div>
+		<div class="large-3 columns contour">
+			<p>Mercredi <br />03 Mai 2017<br /><strong>Médiathèque</strong><br />18h<br /></p>
+			<h3>CIE OUÏE/DIRE</h3>
+		</div>
+		<div class="large-3 columns contour">
+			<p>Mercredi <br />03 Mai 2017<br /><strong>Médiathèque</strong><br />18h<br /></p>
+			<h3>CIE OUÏE/DIRE</h3>
+		</div>
+	</div>
+</section>
 
 <?php get_footer();
